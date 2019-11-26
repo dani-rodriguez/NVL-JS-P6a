76e8d1e89b00f90c80ad8c2ccaf33086ba585354
Daniel Rodriguez <57559169+dani-rodriguez@users.noreply.github.com>
2019-11-26 11:48:01 +0100

    práctica básica js

---
+++ codigo.js
@@ -0,0 +1,10 @@
+var minombre;
+minombre="Daniel"
+var edad;
+edad=33
+var diasdelasemana;
+diasdelasemana="lunes","martes","miercoles","jueves","viernes","sabado","domingo"
+var valorgravedad:
+valorgravedad="g"
+var numerosimpares;
+numerosimpares=1,3,5
